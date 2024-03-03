bb1a625101ae9cc99adb8c6c1035bd93f17188eb
shivamparashar30 <shivamparashar330@gmailcom>
2024-03-03 10:33:21 +0530

    Updated Routes

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const productController = require('../controllers/ProductsControllers');
 
-
 router.get('/', productController.getAllProduct),
 router.get('/:id', productController.getProduct),
 router.get('/search/:key', productController.searchProduct),
